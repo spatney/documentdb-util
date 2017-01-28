1b61492754d060a949d8dfed3c09c6c7b4a4622f
Sachin Patney <spatney@microsoft.com>
2017-01-28 02:13:43 -0800

    fixing interface

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 declare class DocumentDbUtility {
     constructor(config: { authKey: string, host: string });
-    database(databaseId: string): {};
-    collection(database: {}, collectionId: string): {};
-    insert(collection: {}, documentDefinition: {}): {};
-    delete(docLink: string): void;
-    query(collection: {}, querySpec: { query: string, parameters: { name: string, value: string }[] });
-    update(docLink: string, documentDefinition: {});
-    deleteDatabase(databaseId: string);
-    deleteCollection(databaseId: string, collectionId: string);
-    createDocumentLink(databaseId: string, collectionId: string, documentId: string);
+    database(databaseId: string): Promise<{}>;
+    collection(database: {}, collectionId: string): Promise<{}>;
+    insert(collection: {}, documentDefinition: {}): Promise<{}>;
+    delete(docLink: string): Promise<void>;
+    query(collection: {}, querySpec: { query: string, parameters: { name: string, value: string }[] }):Promise<{}>;
+    update(docLink: string, documentDefinition: {}):Promise<{}>;
+    deleteDatabase(databaseId: string):Promise<{}>;
+    deleteCollection(databaseId: string, collectionId: string):Promise<{}>;
+    createDocumentLink(databaseId: string, collectionId: string, documentId: string):string;
 }
 
 export = DocumentDbUtility;
