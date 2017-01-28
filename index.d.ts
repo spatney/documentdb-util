abbde07d6b0e37126e684f99c169e072273522e9
Sachin Patney <spatney@microsoft.com>
2017-01-27 23:13:48 -0800

    adding typings

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+declare class DocumentDbUtility {
+    database(databaseId: string):{};
+    collection(database: {}, collectionId: string):{};
+    insert(collection: {}, documentDefinition: {}):{};
+    delete(docLink: string): void;
+    query(collection:{}, querySpec:{query:string, parameters:{name: string, value: string}[]});
+    update(docLink: string, documentDefinition:{});
+    deleteDatabase(databaseId: string);
+    deleteCollection(databaseId: string, collectionId: string);
+    createDocumentLink(databaseId: string, collectionId: string, documentId: string);
+}
+
+export = DocumentDbUtility;
