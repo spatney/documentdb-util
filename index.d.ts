0a3cc864c31a21f09d61776829fd488d181c642c
Sachin Patney <spatney@microsoft.com>
2017-01-27 23:16:40 -0800

    update typings

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
 declare class DocumentDbUtility {
-    database(databaseId: string):{};
-    collection(database: {}, collectionId: string):{};
-    insert(collection: {}, documentDefinition: {}):{};
+    new(config: { authKey: string, host: string });
+    database(databaseId: string): {};
+    collection(database: {}, collectionId: string): {};
+    insert(collection: {}, documentDefinition: {}): {};
     delete(docLink: string): void;
-    query(collection:{}, querySpec:{query:string, parameters:{name: string, value: string}[]});
-    update(docLink: string, documentDefinition:{});
+    query(collection: {}, querySpec: { query: string, parameters: { name: string, value: string }[] });
+    update(docLink: string, documentDefinition: {});
     deleteDatabase(databaseId: string);
     deleteCollection(databaseId: string, collectionId: string);
     createDocumentLink(databaseId: string, collectionId: string, documentId: string);
