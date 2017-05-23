e0892143a127d1e212292182a5a315292b6da72d
Sachin Patney <spatney@microsoft.com>
2017-05-22 19:35:41 -0700

    making optional

--- index.d.ts
+++ index.d.ts
@@ -4,15 +4,15 @@ declare class DocumentDbUtility {
     collection(database: {}, collectionId: string): Promise<{}>;
     insert(collection: {}, documentDefinition: {}): Promise<{}>;
     delete(docLink: string): Promise<void>;
-    query(collection: {}, querySpec: { query: string, parameters: { name: string, value: string }[] }):Promise<{}>;
-    update(docLink: string, documentDefinition: {}):Promise<{}>;
-    deleteDatabase(databaseId: string):Promise<{}>;
-    deleteCollection(databaseId: string, collectionId: string):Promise<{}>;
-    createDocumentLink(databaseId: string, collectionId: string, documentId: string):string;
-    storedProcedure(collection:{}, proc:{id:string, body: Function, serverScript: Function});
-    executeStoredProcedure(proc:{});
+    query(collection: {}, querySpec: { query: string, parameters: { name: string, value: string }[] }): Promise<{}>;
+    update(docLink: string, documentDefinition: {}): Promise<{}>;
+    deleteDatabase(databaseId: string): Promise<{}>;
+    deleteCollection(databaseId: string, collectionId: string): Promise<{}>;
+    createDocumentLink(databaseId: string, collectionId: string, documentId: string): string;
+    storedProcedure(collection: {}, proc: { id: string, body?: Function, serverScript?: Function });
+    executeStoredProcedure(proc: {});
 }
 
-declare namespace DocumentDbUtility{}
+declare namespace DocumentDbUtility { }
 
 export = DocumentDbUtility;
